83dbf04104d3505a4c482b13af16562c406cf391
Jessica Campos <Jessica.c@bronxsoftware.org>
2023-05-29 14:05:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement("H1");
+
+newHeader.id = "victory";
+newHeader.innerHTML = 'JC is the champion';
+main.appendChild(newHeader);
+
+
+
